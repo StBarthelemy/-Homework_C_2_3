4f203dadf8eedb5e28c99d01f8bc1c0ea127f05a
StBarthelemy <130822796+StBarthelemy@users.noreply.github.com>
2023-05-01 10:02:47 +0300

    готово

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 8)
+{
+    Console.WriteLine("Это не день недели");
+}
+
+else if (number >= 6 && number < 8)
+{
+   Console.WriteLine("Выходной"); 
+}
+
+else if (number >= 1 && number < 7)
+{
+   Console.WriteLine("Рабочий"); 
+}
+
